57716da69a722445670dea83bcac7201767d6cd2
rayamax <chevaux.maxime@hotmail.fr>
2018-09-24 11:50:50 +0200

    avant de faire des conneries

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Article;
 use App\Repository\ArticleRepository;
 
 class ArticleController extends AbstractController
@@ -20,7 +19,6 @@ class ArticleController extends AbstractController
         $articles = $repo->findAll();
 
          return $this->render('article/index.html.twig', [
-            'controller_name' => 'Toto',
              'articles'=>$articles
         ]);
     }
@@ -29,14 +27,14 @@ class ArticleController extends AbstractController
      * @Route("/article/new", name="article_create")
      */
     public function createArticle(){
+
         return $this->render('article/create.html.twig');
     }
 
     /**
      * @Route("/article/{id}", name="article_show")
      */
-    public function show($id){
-        $repo = $this->getDoctrine()->getRepository(Article::class);
+    public function show($id, ArticleRepository $repo){
 
         $article = $repo->find($id);
 
