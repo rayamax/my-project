cd9c9a819b167a464892e6c8c00a521b9b61c855
rayamax <chevaux.maxime@hotmail.fr>
2018-10-29 15:47:33 +0100

    mise en page + json classement + rencontres

--- public/js/indexresults.js
+++ public/js/indexresults.js
@@ -1,6 +1,7 @@
-const indexResults = document.getElementById('index-results');
-const urlResults = "https://www.thesportsdb.com/api/v1/json/1/eventsnextleague.php?id=4334";
-fetch(urlResults)
+const calendar = document.getElementById('calendar');
+const results = document.getElementById('results');
+const urlCalendar = "https://www.thesportsdb.com/api/v1/json/1/eventsnextleague.php?id=4334";
+fetch(urlCalendar)
     .then((resp)=>resp.json())
     .then(function(data) {
         let teams = data.events;
@@ -9,15 +10,39 @@ fetch(urlResults)
             let tr = createNode('tr'),
                 th1 = createNode('th'),
                 th2 = createNode('th'),
-                th3 = createNode('th');
+                th3 = createNode('th'),
+                date = team.strDate.substr(0,5),
+                time = team.strTime.substr(0,5);
             stand++;
+
             th1.innerHTML = `${team.strHomeTeam}`;
-            th2.innerHTML = `${team.dateEvent}`;
+            th2.innerHTML = `${date} <br> ${time}`;
             th3.innerHTML = `${team.strAwayTeam}`;
-            append(indexResults, tr);
+            append(results, tr);
             append(tr, th1);
             append(tr, th2);
             append(tr, th3);
+        })
+    })
+const urlResults = "https://www.thesportsdb.com/api/v1/json/1/eventspastleague.php?id=4334";
+fetch(urlResults)
+    .then((resp)=>resp.json())
+    .then(function(data) {
+        let teams = data.events;
+        return teams.map(function(team){
+            let tr = createNode('tr'),
+                th1 = createNode('th'),
+                th2 = createNode('th'),
+                th3 = createNode('th');
+
+            th1.innerHTML = `${team.strHomeTeam}`;
+            th2.innerHTML = `${team.intHomeScore} - ${team.intAwayScore}`;
+            th3.innerHTML = `${team.strAwayTeam}`;
+            if (team.intAwayScore != null){
+                append(calendar, tr);
+                append(tr, th1);
+                append(tr, th2);
+                append(tr, th3);}
         })
     })
     .catch(function(error) {
